939652237a687749e06f84578728fc3dc2709a59
jin xm <jinxm@jindeMacBook-Pro.local>
2026-08-14 17:47:28 +0800

    fix: 移除官方新版不存在的 Tooltip compact prop

--- excalidraw-app/components/AppFooter.tsx
+++ excalidraw-app/components/AppFooter.tsx
@@ -55,7 +55,7 @@ const AppFooterLeft = React.memo(
       <FooterLeftExtraTunnel.In>
         <div className="footer-left-extra zen-mode-transition">
           <div className="excalidraw-tooltip-wrapper">
-            <Tooltip label="画布列表" compact>
+            <Tooltip label="画布列表">
               <button
                 className="sidebarButton"
                 onClick={handleToggleCreationsSidebar}
